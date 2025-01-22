44ed070f176056b1f2fa45372f5a15bb5028f419
sahilnaiktutorial <naiksahil478@gmail.com>
2025-01-22 17:26:52 +0530

    Added  RestResponseBuilder

---
+++ src/main/java/com/example/bsm/utility/RestResponseBuilder.java
@@ -0,0 +1,30 @@
+package com.example.bsm.utility;
+
+import org.springframework.http.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestResponseBuilder {
+
+    public  <T> ResponseEntity<ResponseStructure<T>> success(HttpStatus status,String message,T data){
+        return ResponseEntity
+                .status(status)
+                .body(ResponseStructure.<T>builder()
+                        .httpStatus(status)
+                        .message(message)
+                        .data(data)
+                        .build());
+    }
+
+    public  <T> ResponseEntity<ErrorStructure> error(HttpStatus status,String message,String rootcause){
+        return ResponseEntity
+                .status(status)
+                .body(ErrorStructure.builder()
+                        .status(status.value())
+                        .message(message)
+                        .rootCause(rootcause)
+                        .build());
+    }
+
+}
