fe530e7b0d7690f3580a6dba89899596fbb2310b
sahilnaiktutorial <naiksahil478@gmail.com>
2025-01-22 17:26:13 +0530

    Added  ErrorStructure

---
+++ src/main/java/com/example/bsm/utility/ErrorStructure.java
@@ -0,0 +1,20 @@
+package com.example.bsm.utility;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorStructure {
+
+    private int status;
+    private String message;
+    private String rootCause;
+
+}
