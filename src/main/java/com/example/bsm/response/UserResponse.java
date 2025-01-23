03351e7e19f7bd169f42ebc84a94fc7f98f37c4f
sahilnaiktutorial <naiksahil478@gmail.com>
2025-01-23 12:44:50 +0530

    Added UserResponse DTO

--- src/main/java/com/example/bsm/response/UserResponse.java
+++ src/main/java/com/example/bsm/response/UserResponse.java
@@ -1,4 +1,26 @@
 package com.example.bsm.response;
 
+import com.example.bsm.enums.BloodGroup;
+import com.example.bsm.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserResponse {
+
+    private int userId;
+    private String username;
+    private BloodGroup bloodGroup;
+    private Date lastDenotedDate;
+    private int age;
+    private Gender gender ;
+    private String availableCity;
+    private boolean verified;
 }
