202ff3765f5754a3f92534a869ff1ab49b2f812c
sahilnaiktutorial <naiksahil478@gmail.com>
2025-01-23 12:42:15 +0530

    Added UserRequest DTO

--- src/main/java/com/example/bsm/request/UserRequest.java
+++ src/main/java/com/example/bsm/request/UserRequest.java
@@ -1,4 +1,25 @@
 package com.example.bsm.request;
 
+
+import com.example.bsm.enums.BloodGroup;
+import com.example.bsm.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRequest {
+
+    private String username;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private int age;
+    private Gender gender ;
+    private String availableCity;
+    private BloodGroup bloodGroup;
 }
