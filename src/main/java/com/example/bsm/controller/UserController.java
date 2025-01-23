25b72911d56c5a33f19735b995c0cf7581d654f9
sahilnaiktutorial <naiksahil478@gmail.com>
2025-01-23 12:51:07 +0530

    in  controller class changed returntype to userResponse and parameters to UserRequest

--- src/main/java/com/example/bsm/controller/UserController.java
+++ src/main/java/com/example/bsm/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.bsm.controller;
 
 import com.example.bsm.entity.User;
+import com.example.bsm.request.UserRequest;
+import com.example.bsm.response.UserResponse;
 import com.example.bsm.service.UserService;
 import com.example.bsm.utility.ResponseStructure;
 import com.example.bsm.utility.RestResponseBuilder;
@@ -24,19 +26,19 @@ private RestResponseBuilder restResponseBuilder;
 
 
     @PostMapping("/register")
-    public ResponseEntity<ResponseStructure<User>> addUser(@RequestBody User user){
-        User newUser = userService.addUser(user);
-        return restResponseBuilder.success(HttpStatus.CREATED,"user created",newUser);
+    public ResponseEntity<ResponseStructure<UserResponse>> addUser(@RequestBody UserRequest userRequest){
+        UserResponse user = userService.addUser(userRequest);
+        return restResponseBuilder.success(HttpStatus.CREATED,"user created",user);
     }
 
     @GetMapping("/users/{userId}")
-    public ResponseEntity<ResponseStructure<User>> findUserById(@PathVariable int userId){
-        User user = userService.findUserById(userId);
+    public ResponseEntity<ResponseStructure<UserResponse>> findUserById(@PathVariable int userId){
+        UserResponse user = userService.findUserById(userId);
         return restResponseBuilder.success(HttpStatus.FOUND,"user found",user);
     }
     @PutMapping("/users/{userid}")
-    public ResponseEntity<ResponseStructure<User>> updateActor(@PathVariable("userid") int userId,@RequestBody User user) {
-         User updateUser=userService.updateUser(userId,user);
+    public ResponseEntity<ResponseStructure<UserResponse>> updateUser(@PathVariable("userid") int userId,@RequestBody UserRequest user) {
+        UserResponse updateUser=userService.updateUser(userId,user);
         return restResponseBuilder.success(HttpStatus.ACCEPTED,"user updated",updateUser);
     }
 
