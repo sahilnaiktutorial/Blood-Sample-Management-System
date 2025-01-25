1bbe73d269c001fb4e3099abb3dfbf41c4a59fc5
sahilnaiktutorial <naiksahil478@gmail.com>
2025-01-25 16:31:08 +0530

    added handleUserNotFound  to handle UserNotFoundByIdException and handleUserNotFound to handle UsernameNotFoundException

--- src/main/java/com/example/bsm/exceptionhandler/UserExceptionHandler.java
+++ src/main/java/com/example/bsm/exceptionhandler/UserExceptionHandler.java
@@ -2,11 +2,11 @@ package com.example.bsm.exceptionhandler;
 
 import com.example.bsm.exception.UserNotFoundByIdException;
 import com.example.bsm.utility.ErrorStructure;
-import com.example.bsm.utility.ResponseStructure;
 import com.example.bsm.utility.RestResponseBuilder;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,7 +17,12 @@ public class UserExceptionHandler {
     private final RestResponseBuilder restResponseBuilder;
 
     @ExceptionHandler(UserNotFoundByIdException.class)
-    public ResponseEntity<ErrorStructure> handleUserNotFoundById(UserNotFoundByIdException ex){
+    public  ResponseEntity<ErrorStructure<String>> handleUserNotFound(UserNotFoundByIdException ex){
         return restResponseBuilder.error(HttpStatus.NOT_FOUND,ex.getMessage(),"user not found");
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public  ResponseEntity<ErrorStructure<String>> handleUserNotFound(UsernameNotFoundException ex){
+        return restResponseBuilder.error(HttpStatus.NOT_FOUND,ex.getMessage(),"failed to authenticate user");
     }
 }
